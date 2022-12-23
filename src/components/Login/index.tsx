bfac4d23d6aa4e363ecf66bc1ed1a65cca70492c
= <=>
2022-12-23 22:26:28 +0700

    five update

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -2,31 +2,31 @@ import React from 'react';
 import '../../../src/index.scss'
 import { Button, Form, Input } from 'antd'
 import '../Login/login.scss'
-import logo from '../../assets/image/Rectangle 337.png'
-import loginImage from '../../assets/image/Artboard-113_WF-Automation-illustrations 1 (1).png'
+import logo from '../../assets/image/login-logo.png'
+import loginImage from '../../assets/image/login-image.png'
 export const Login = () => {
   console.log('login page');
   return (
     <div className='login'>
-      <div className='login__wrapper'>
-        <div className='login__wrapper--left'>
+      <div className="login__wrapper">
+        <div className="login__wrapper__left">
           <img src={loginImage} alt="" />
         </div>
-        <div className='login__wrapper--right'>
-          <div className='logo'>
-            <img className='logo__img' src={logo}></img>
-            <div className='logo__text'>ZARVIS</div>
+        <div className="login__wrapper__right">
+          <div className="logo">
+            <img className="logo__img" src={logo}></img>
+            <div className="logo__text">ZARVIS</div>
           </div>
           <Form layout="vertical" >
-            <div className='login__input'>
-              <label className='login__input--label'>Username</label>
+            <div className="login__input">
+              <label className="login__input__label">Username</label>
               <Input placeholder='Enter your username'></Input>
             </div>
-            <div className='login__input'>
-              <label className='login__input--label'>Password</label>
+            <div className="login__input">
+              <label className="login__input__label">Password</label>
               <Input type='password' placeholder='Enter your password'></Input>
             </div>
-            <Button className='login__btn'>Login</Button>
+            <Button className="login__btn">Login</Button>
           </Form>
         </div>
       </div>
