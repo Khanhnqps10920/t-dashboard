8a62bb81a17730441f8ed42add0b3df993ab01c9
= <=>
2022-12-23 12:30:58 +0700

    frist commit

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,6 +1,34 @@
 import React from 'react';
-
+import { Button, Form, Input } from 'antd'
+import '../Login/login.scss'
+import logo from '../../assets/image/Rectangle 337.png'
+import loginImage from '../../assets/image/Artboard-113_WF-Automation-illustrations 1 (1).png'
 export const Login = () => {
   console.log('login page');
-  return <div>Login page</div>;
+  return <div>
+    <div className='login'>
+      <div className='login-wrapper'>
+        <div className='login-wrapper-left'>
+          <img src={loginImage} alt="" />
+        </div>
+        <div className='login-wrapper-right'>
+          <div className='logo'>
+            <img className='logo-img' src={logo}></img>
+            <div className='logo-text'>ZARVIS</div>
+          </div>
+          <Form layout="vertical" >
+            <div className='login-input'>
+              <label>Username</label>
+              <Input placeholder='Enter your username'></Input>
+            </div>
+            <div className='login-input'>
+              <label>Password</label>
+              <Input className='input-' type='password' placeholder='Enter your password'></Input>
+            </div>
+            <Button className='login-btn'>Login</Button>
+          </Form>
+        </div>
+      </div>
+    </div>
+  </div>;
 };
