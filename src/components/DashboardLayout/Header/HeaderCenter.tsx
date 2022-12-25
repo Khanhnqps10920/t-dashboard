16a480f9ed11f3c232f4ca9381fdf3b68228cb9c
Cuong Nguyen <ntc2019@gmail.com>
2022-12-25 12:30:02 +0700

    Change px to rem. Add color var. Svg inline to import. Add cleanup function for useEffect.

--- src/components/DashboardLayout/Header/HeaderCenter.tsx
+++ src/components/DashboardLayout/Header/HeaderCenter.tsx
@@ -1,12 +1,9 @@
-
+import searchIcon from '../../../assets/image/header-search-icon.svg'
 const HeaderCenter = () => {
     return (
         <div className="header__center">
             <div className="search-icon">
-                <svg viewBox="0 0 24 24" fill="none">
-                    <path d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z" stroke="#AEB6CF" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                    <path d="M22 22L20 20" stroke="#AEB6CF" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                </svg>
+                <img src={searchIcon} />
             </div>
             <input className='search-input' type="text" placeholder='Search' />
         </div>
--- src/components/DashboardLayout/Header/HeaderLeft.tsx
+++ src/components/DashboardLayout/Header/HeaderLeft.tsx
@@ -6,13 +6,16 @@ const HeaderLeft = () => {
 
     const [date, setDate] = useState(getDate());
     useEffect(() => {
-        setInterval(() => { setDate(getDate()) }, 2000)
+        const updateTime = setInterval(() => { setDate(getDate()) }, 2000)
+        return () => {
+            clearInterval(updateTime)
+        }
     }, [])
     // name will be retrieved from context api
     const name = 'Kruluz Utsman'
     return (
         <div className="header__left">
-            <div className="hello">{`Hello ${name}`}</div>
+            <div className="hello">Hello {name}</div>
             <div className="date">{date}</div>
         </div>
     )
