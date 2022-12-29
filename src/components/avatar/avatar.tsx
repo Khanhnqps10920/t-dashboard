0412ada482e3a48e795dc2cedbccea88276a452e
tan thanh <dangtanthanh12a2@gmail.com>
2022-12-29 15:19:24 +0700

    add avatar.scss

--- src/components/avatar/avatar.tsx
+++ src/components/avatar/avatar.tsx
@@ -1,20 +1,7 @@
 // demo:<Avatar email="admin@gmail.com"/>
-
+import "./avatar.scss"
 const Avatar=({email=''})=>{
-    const styleBox={
-        width:'100%',
-        height:'100%',
-        background:'#00FF7F',
-        borderRadius:'50%',
-        color:'white',
-        fontSize:'5.5rem',
-        display:'flex',
-        justifyContent:'center',
-        alignItems:'center',
-        overflow:'hidden',
-        fontWeight:'600',
-    }
-    return <div style={styleBox}>
+    return <div className="e-avatar">
         {email.slice(0,2).toUpperCase()}
     </div>
 }
