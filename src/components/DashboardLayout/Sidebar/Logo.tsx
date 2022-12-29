17d4545a4ebe37034d213ea4cbcdbfba9225a233
Khanh Nguyen <khanhng3009@gmail.com>
2022-12-30 00:06:48 +0700

    fix wrong image name

--- src/components/DashboardLayout/Sidebar/Logo.tsx
+++ src/components/DashboardLayout/Sidebar/Logo.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles/Logo.scss';
-import logo from './../../../assets/logo.png';
+import logo from './../../../assets/Logo.png';
 const Logo = () => {
   return (
     <div className="logo">
